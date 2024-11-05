6b07c35f3fcd6ef4b266116657a2437ff47f3b31
chrissu777 <christophersu777@gmail.com>
2024-11-05 00:14:16 -0500

    what happend

--- app/src/main/java/com/example/dndpromax21/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/dndpromax21/ui/screens/MainScreen.kt
@@ -1,10 +1,25 @@
 package com.example.dndpromax21.ui.screens
+
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.tooling.preview.Preview
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun MainScreen(){
-    Text("Balls")
+fun MainScreen() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Small Top App Bar")
+                }
+            )
+        },
+    ) {
+    }
 }
