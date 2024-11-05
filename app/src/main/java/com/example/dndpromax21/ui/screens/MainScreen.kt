eac51b814c350385cecd71db238b7d3e1c6d2274
chrissu777 <christophersu777@gmail.com>
2024-11-04 23:50:53 -0500

    main screen test

--- app/src/main/java/com/example/dndpromax21/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/dndpromax21/ui/screens/MainScreen.kt
@@ -1,2 +1,10 @@
 package com.example.dndpromax21.ui.screens
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.tooling.preview.Preview
 
+@Preview
+@Composable
+fun MainScreen(){
+    Text("Balls")
+}
